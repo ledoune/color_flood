dd8c949a06790b25e23f5bd68ace85492a568639
Doune00 <pierre.denes00@gmail.com>
2017-03-10 11:48:31 +0100

    TODO grid

--- grid.h
+++ grid.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "colors.h"
+#include <stdbool.h>
 
 typedef struct t_grid {
 	RGB* rgbGrid;
@@ -14,7 +15,13 @@ grid *gridInit(int size);
 void gridInitColors(grid *g, RGB *cTab, int cNb);
 void gridFree(grid *g);
 /*
- * TODO : gridSetColors(grid *g, RGB newColor, coord);
+ * TODO : return int for errors ?
+ * int gridSetColors(grid *g, RGB newColor, int x, int y);
+ * RGB gridGetColor(grid *g, int x, int y);
+ * type? gridPlayerGetCC(grid *g, RGB playerColor);
+ * int gridPlayerSetColor(type? CC, RGB newColor);
+ * bool gridPlayerVictory(grid *g);
+ *
  */ 
 
 #endif
